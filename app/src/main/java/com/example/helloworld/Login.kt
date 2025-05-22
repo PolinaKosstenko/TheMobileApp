8968858824c3fefa304766dfa610619f8e437942
Костенко Полина Олеговна <Poli.na-Kostenko@yandex.ru>
2025-05-22 13:06:54 +1000

    add login screen

---
+++ app/src/main/java/com/example/helloworld/Login.kt
@@ -0,0 +1,33 @@
+package com.example.helloworld
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // Активация кнопки "Назад" в ActionBar
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
+        val button = findViewById<Button>(R.id.bthButton)
+        button.setOnClickListener {
+            val intent = Intent(this, WelcomeScreen::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean { //функция вызывается при нажатии на кнопку Назад в шапке
+        finish() // Закрываем Activity
+        return true //зачем то возвращаем
+    }
+}
